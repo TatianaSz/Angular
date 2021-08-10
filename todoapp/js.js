e34adf4f86e9244c44b5034a318e4550ee1b6eaf
Tatiana <Tatiszulik777@gmail.com>
2021-08-10 11:47:31 +0200

    added simple task

--- todoapp/js.js
+++ todoapp/js.js
@@ -1,6 +1,14 @@
 const app = angular.module("app",[]);
 app.controller("date", ['$scope', function($scope){
     let today=new Date();
-    let todayDate = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+ today.getDate();
+    let todayDate = today.getFullYear()+"-"+(today.getMonth()<10?"0"+parseInt(today.getMonth()+1):today.getMonth()+1)+"-"+ (today.getDate()<10?"0"+today.getDate():today.getDate());
     $scope.message= "What will you do today? \n" +todayDate;
+}])
+
+app.controller("taski", ["$scope", function($scope){
+    $scope.tasks = [
+        {taskName: "Fill my task list!",
+         hour: "Everyday!"
+        },
+    ]
 }])
